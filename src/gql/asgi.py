6f177a41662fa686dad9613744eb1baaccea1c65
Brian Edwards <brian@edwardstx.us>
2023-09-11 21:34:33 -0500

    mutation

--- src/gql/asgi.py
+++ src/gql/asgi.py
@@ -1,15 +1,17 @@
 from ariadne import QueryType, load_schema_from_path, make_executable_schema
 from ariadne.asgi import GraphQL
-from .resolver import Resolver
+from .resolver import Query, MutationResolver
 
 
 def graph_api():
     type_defs = load_schema_from_path("./src/gql/")
 
-    query = QueryType()
-    r = Resolver("Hello?", query)
+    query_resolver = Query("Hi!")
+    query = query_resolver.query
+    mutation_resolver = MutationResolver()
+    mutations = mutation_resolver.mutation
 
-    schema = make_executable_schema(type_defs, query)
+    schema = make_executable_schema(type_defs, query, mutations)
     app = GraphQL(schema, debug=True)
 
     return app
--- src/gql/resolver.py
+++ src/gql/resolver.py
@@ -1,4 +1,31 @@
-from ariadne import QueryType
+from ariadne import QueryType, MutationType
+from models import Colors
+
+
+class Query:
+    def __init__(self, name: str) -> None:
+        self.query = QueryType()
+        self.name = name
+        self.query.set_field("hello", self.resolve_hello)
+
+    def resolve_hello(self, *_):
+        return self.name
+
+
+class MutationResolver:
+    def __init__(self) -> None:
+        self.mutation = MutationType()
+        self.mutation.set_field("greet", self.resolve_greet)
+        self.mutation.set_field("display", self.resolve_display)
+
+    def resolve_greet(self, _, info, name: str):
+        return f"Hello {name}"
+
+    def resolve_display(self, _, info, input: dict):
+        colors = Colors(**input)
+        print(colors.model_dump())
+
+        return "0"
 
 
 class Resolver:
